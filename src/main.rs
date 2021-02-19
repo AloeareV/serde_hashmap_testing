4ffa4b73d1cf7882c916f3fdb930ce861b63a6fe
Hazel OHearn <gygaxis@protonmail.com>
2021-02-19 15:43:54 -0400

    Holy shit it just works

--- src/main.rs
+++ src/main.rs
@@ -32,7 +32,7 @@ pub struct Upgrade {
     pub status: String,
 }
 #[derive(Debug, serde :: Deserialize, serde :: Serialize)]
-pub struct TEST_record_interpretation_getblockchaininfo {
+pub struct Getblockchaininfo {
     pub bestblockhash: String,
     pub blocks: rust_decimal::Decimal,
     pub chain: String,
@@ -42,11 +42,21 @@ pub struct TEST_record_interpretation_getblockchaininfo {
     pub difficulty: rust_decimal::Decimal,
     pub estimatedheight: rust_decimal::Decimal,
     pub headers: rust_decimal::Decimal,
-    pub initial_block_download_complete: bool,
+    pub initial_block_download_complete: Option<bool>,
     pub size_on_disk: rust_decimal::Decimal,
     pub softforks: Vec<Softforks>,
     pub upgrades: std::collections::HashMap<String, Upgrade>,
     pub verificationprogress: rust_decimal::Decimal,
 }
 
-fn main() {}
+fn main() {
+    let file = std::fs::read("exampledata.json").unwrap();
+    let test: Getblockchaininfo =
+        serde_json::from_str(&String::from_utf8_lossy(&file)).unwrap();
+    println!("{:#?}", test);
+    dbg!("{:#?}", type_name_of_val(test.upgrades));
+}
+
+fn type_name_of_val<T>(_: T) -> String {
+    std::any::type_name::<T>().to_string()
+}
